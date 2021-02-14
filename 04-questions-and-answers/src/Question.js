9a5f00aea459e0c1300c0ab67baa55457f30fb4e
Youpele Michael <mjayjesus@gmail.com>
2021-02-14 23:32:21 +0100

    questions and answers project added

---
+++ 04-questions-and-answers/src/Question.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+// some icons
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
+const Question = ({ title, info }) => {
+  const [display, setDisplay] = useState(false)
+  return (
+    <article className='question'>
+      <header>
+        <h4> {title}</h4>
+        <button className='btn' onClick={() => setDisplay(!display)}>
+          {display ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {display ? <p>{info}</p> : ''}
+    </article>
+  )
+}
+
+export default Question
